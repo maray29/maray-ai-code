911ab7fc5b48641fbcd80981c6cfeebff2d2548f
maray29 <63769810+maray29@users.noreply.github.com>
2024-03-16 00:00:51 +0100

    Fixed cursor animation bug

--- src/template-gallery/index.js
+++ src/template-gallery/index.js
@@ -5,7 +5,7 @@ import createLenis from '$utils/createLenis';
 function init() {
   createLenis();
   changeTheme("[data-element='theme-toggle']");
-  animateCursor('.cursor_inner', mouse, 0.2);
+  animateCursor('.cursor_inner');
   animateCursorElements([
     '[data-element="nav-dropdown-link"]',
     '[data-element="nav-articles-item"]',
